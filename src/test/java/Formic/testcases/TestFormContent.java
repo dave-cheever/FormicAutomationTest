59a68014ab209f46d0ecdde1ee9d8b93372e5927
DaveCheever <dave.cheever@formic.co.uk>
2023-09-12 18:40:15 +0800

    updated azure pipelines

--- src/test/java/Formic/testcases/TestFormContent.java
+++ src/test/java/Formic/testcases/TestFormContent.java
@@ -32,98 +32,98 @@ public class TestFormContent extends BaseUiTest {
         checkBoxPage.validateEnableDisableFieldsCheckbox();
     }
 
-    @Test
-    public void validateMandatoryFieldsWithoutInputs() throws Exception{
-        homePage.selectProject(projectName);
-        checkBoxPage.validateCheckboxMandatory();
-    }
-
-    @Test
-    public void validateMinimumRequiredWhenSubmitButtonClicked() throws Exception{
-        homePage.selectProject(projectName);
-        checkBoxPage.validateCheckboxMinimumValidationUponSubmit();
-    }
-
-    @Test
-    public void checkboxValidationBeyondMaximumInputs() throws Exception{
-        homePage.selectProject(projectName);
-        checkBoxPage.validateCheckboxBeyondMaximumInputsUponSubmit();
-    }
-
-    @Test
-    public void checkboxValidationWithinMaximumInputs() throws Exception{
-        homePage.selectProject(projectName);
-        checkBoxPage.validateCheckboxMaximumInputsWithinLimitUponSubmit();
-    }
-
-    @Test
-    public void checkboxValidationWithinMinimumInputs() throws Exception{
-        homePage.selectProject(projectName);
-        checkBoxPage.validateCheckboxWithinMinimumInputsUponSubmit();
-    }
-
-    @Test
-    public void checkboxValidationLessThanMinimumInputs() throws Exception{
-        homePage.selectProject(projectName);
-        checkBoxPage.validateCheckboxLessThanMinimumInputsUponSubmit();
-    }
-
-    @Test
-    public void hroFormatValidation() throws Exception{
-        homePage.selectProject(projectName);
-        checkBoxPage.hroFormatValidation();
-    }
-
-    @Test
-    public void miaFormatValidation() throws Exception{
-        homePage.selectProject(projectName);
-        checkBoxPage.miaFormatValidation();
-    }
-
-    @Test
-    public void hroMaximumInputsValidation() throws Exception{
-        //hro validation message
-        homePage.selectProject(projectName);
-        checkBoxPage.hroMaximumInputsValidation();
-    }
-
-    @Test
-    public void miaMandatoryValidation() throws Exception{
-        homePage.selectProject(projectName);
-        checkBoxPage.miaMandatoryValidation();
-    }
-
-    @Test
-    public void miaPicklistLessThanMinimumInputs() throws Exception{
-        homePage.selectProject(projectName);
-        checkBoxPage.miaPicklistLessThanMinimumInputs();
-    }
-
-    @Test
-    public void miaPicklistMoreThanMaximumInputs() throws Exception{
-        homePage.selectProject(projectName);
-        checkBoxPage.miaPicklistMoreThanMaximumInputs();
-    }
-
-    @Test
-    public void miaPicklistWithinMinimumMaximumInputs() throws Exception{
-        homePage.selectProject(projectName);
-        checkBoxPage.miaPicklistWithinMinimumMaximumInputs();
-    }
-
-
-
-    @Test
-    public void validateSubmittedInputsCheckbox() throws Exception {
-        homePage.selectProject(projectName);
-        checkBoxPage.validateSubmittedInputsCheckbox();
-    }
-
-    @Test
-    public void validateSavedInputsCheckbox() throws Exception {
-        homePage.selectProject(projectName);
-        checkBoxPage.validateSavedInputsCheckbox();
-    }
+//    @Test
+//    public void validateMandatoryFieldsWithoutInputs() throws Exception{
+//        homePage.selectProject(projectName);
+//        checkBoxPage.validateCheckboxMandatory();
+//    }
+//
+//    @Test
+//    public void validateMinimumRequiredWhenSubmitButtonClicked() throws Exception{
+//        homePage.selectProject(projectName);
+//        checkBoxPage.validateCheckboxMinimumValidationUponSubmit();
+//    }
+//
+//    @Test
+//    public void checkboxValidationBeyondMaximumInputs() throws Exception{
+//        homePage.selectProject(projectName);
+//        checkBoxPage.validateCheckboxBeyondMaximumInputsUponSubmit();
+//    }
+//
+//    @Test
+//    public void checkboxValidationWithinMaximumInputs() throws Exception{
+//        homePage.selectProject(projectName);
+//        checkBoxPage.validateCheckboxMaximumInputsWithinLimitUponSubmit();
+//    }
+//
+//    @Test
+//    public void checkboxValidationWithinMinimumInputs() throws Exception{
+//        homePage.selectProject(projectName);
+//        checkBoxPage.validateCheckboxWithinMinimumInputsUponSubmit();
+//    }
+//
+//    @Test
+//    public void checkboxValidationLessThanMinimumInputs() throws Exception{
+//        homePage.selectProject(projectName);
+//        checkBoxPage.validateCheckboxLessThanMinimumInputsUponSubmit();
+//    }
+//
+//    @Test
+//    public void hroFormatValidation() throws Exception{
+//        homePage.selectProject(projectName);
+//        checkBoxPage.hroFormatValidation();
+//    }
+//
+//    @Test
+//    public void miaFormatValidation() throws Exception{
+//        homePage.selectProject(projectName);
+//        checkBoxPage.miaFormatValidation();
+//    }
+//
+//    @Test
+//    public void hroMaximumInputsValidation() throws Exception{
+//        //hro validation message
+//        homePage.selectProject(projectName);
+//        checkBoxPage.hroMaximumInputsValidation();
+//    }
+//
+//    @Test
+//    public void miaMandatoryValidation() throws Exception{
+//        homePage.selectProject(projectName);
+//        checkBoxPage.miaMandatoryValidation();
+//    }
+//
+//    @Test
+//    public void miaPicklistLessThanMinimumInputs() throws Exception{
+//        homePage.selectProject(projectName);
+//        checkBoxPage.miaPicklistLessThanMinimumInputs();
+//    }
+//
+//    @Test
+//    public void miaPicklistMoreThanMaximumInputs() throws Exception{
+//        homePage.selectProject(projectName);
+//        checkBoxPage.miaPicklistMoreThanMaximumInputs();
+//    }
+//
+//    @Test
+//    public void miaPicklistWithinMinimumMaximumInputs() throws Exception{
+//        homePage.selectProject(projectName);
+//        checkBoxPage.miaPicklistWithinMinimumMaximumInputs();
+//    }
+//
+//
+//
+//    @Test
+//    public void validateSubmittedInputsCheckbox() throws Exception {
+//        homePage.selectProject(projectName);
+//        checkBoxPage.validateSubmittedInputsCheckbox();
+//    }
+//
+//    @Test
+//    public void validateSavedInputsCheckbox() throws Exception {
+//        homePage.selectProject(projectName);
+//        checkBoxPage.validateSavedInputsCheckbox();
+//    }
 
 //    @Test
 //    public void test() throws Exception {
