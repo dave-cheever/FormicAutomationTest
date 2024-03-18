0314c0c79bce45616aee017492c15ce65ce5e54e
DaveCheever <dave.cheever@formic.co.uk>
2024-03-18 18:51:10 +0800

    updated yml file

--- src/main/java/com/Formic/OF2/utils/ScreenshotHelper.java
+++ src/main/java/com/Formic/OF2/utils/ScreenshotHelper.java
@@ -13,7 +13,7 @@ import java.util.Date;
 
 public class ScreenshotHelper extends BasePage {
 
-    private static final String SCREENSHOT_FOLDER = "./src/test/java/screenShots/";
+    private static final String SCREENSHOT_FOLDER = "./src/test/java/Formic/screenShots/";
 
     public ScreenshotHelper(WebDriver driver) {
         super(driver);
