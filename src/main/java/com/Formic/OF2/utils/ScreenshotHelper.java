670aeb89f700da2afbe0165767bca89aad7d5ee4
DaveCheever <dave.cheever@formic.co.uk>
2024-03-19 18:15:36 +0800

    updated pom file and yml file for screenshots

--- src/main/java/com/Formic/OF2/utils/ScreenshotHelper.java
+++ src/main/java/com/Formic/OF2/utils/ScreenshotHelper.java
@@ -13,7 +13,7 @@ import java.util.Date;
 
 public class ScreenshotHelper extends BasePage {
 
-    private static final String SCREENSHOT_FOLDER = "./screenShots";
+    private static final String SCREENSHOT_FOLDER = "screenShots/";
 
     public ScreenshotHelper(WebDriver driver) {
         super(driver);
